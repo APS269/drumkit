a2b037805072d350edb50e7ac723e06f490b5626
Abhay Pratap Singh <89133831+APS269@users.noreply.github.com>
2022-02-26 11:16:01 +0530

    drum_initial

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,40 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  var binh=this.innerHTML;
+ makeSound(binh);
+});
+}
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+});
+function makeSound(key)
+{
+  switch(key)
+  {
+    case "w":var t1=new Audio("sounds/tom-1.mp3");
+    t1.play();
+    break;
+    case "a":var t2=new Audio("sounds/tom-2.mp3");
+    t2.play();
+    break;
+    case "s":var t3=new Audio("sounds/tom-3.mp3");
+    t3.play();
+    break;
+    case "d":var t4=new Audio("sounds/tom-4.mp3");
+    t4.play();
+    break;
+    case "j":var snare=new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+    case "k":var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+    case "l":var kick=new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+    default:
+  console.log(binh);
+     break;
+}
+//What to do when click
+}
